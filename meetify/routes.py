01bf8c88c1ed4b3d795517da4ff6026c3fe40de9
AryanKhurana <64011374+AryanKhurana@users.noreply.github.com>
2020-07-07 16:29:28 +0530

    Update routes.py

--- meetify/routes.py
+++ meetify/routes.py
@@ -118,6 +118,10 @@ def forgot_password():
 def logout():
     logout_user()
     return redirect(url_for('home'))
+#########################################################################################
+@app.errorhandler(404) 
+def invalid_route(e): 
+    return render_template('404.html')
 
 ##########################################################################################
 
